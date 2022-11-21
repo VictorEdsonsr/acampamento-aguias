bdf2a703a76dc26d2f030c7899a635b32a8d1bb4
Victor <edvictorsr@gmail.com>
2022-11-20 21:04:39 -0400

    modificacoes na pagina contato

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -11,17 +11,14 @@ export default function Contato() {
       </Head>
       <main>
         <section className="flex flex-col items-center justify-center">
-          <div className="text-6xl md:text-9xl font-bold text-orange-200 relative">
-            CONTATO
-            <div className="text-2xl md:text-5xl font-bold text-orange-500 left-[60px] md:left-32 bottom-0 absolute">
-              ACAMPAMENTO
-            </div>
+          <div className="text-6xl md:text-9xl mt-10 font-bold text-orange-500 ">
+            CONTATOS
           </div>
         </section>
 
-        <section className="flex justify-center item-center h-[600px] my-20 sm:my-0">
+        <section className="flex justify-center item-center h-[600px] my-12 sm:my-0">
           <div className="container mx-auto max-w-2xl flex flex-col justify-between items-center h-full sm:flex-row">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-40">
               <FaPhone size={50} />
               <h1 className="my-6 font-semibold text-xl">Telefone</h1>
               <Link
@@ -32,13 +29,13 @@ export default function Contato() {
               </Link>
             </div>
 
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col sm:mx-5 items-center w-40">
               <FaWhatsappSquare size={50} />
               <h1 className="my-6 font-semibold text-xl">WhatsApp</h1>
               <WhatsappButton>CONVERSAR NO WHATSAPP</WhatsappButton>
             </div>
 
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-40">
               <FaEnvelope size={50} />
               <h1 className="my-6 font-semibold text-xl">Email</h1>
               <Link
